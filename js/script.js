a6f8d03094368ec94ec6ea23efbe1106692ff907
Dominik <dominik.krizs@gmail.com>
2024-02-27 11:19:10 +0100

    set active menu links

--- js/script.js
+++ js/script.js
@@ -1,3 +1,10 @@
+$(document).ready(function(){
+   // set active links
+   $('#menu_links li a').click(function(){
+     $('#menu_links li a').removeClass('text-blue-700');
+     $(this).addClass('text-blue-700');
+   });
+ });
 
 window.addEventListener('scroll', function () {
     isNavbarVisible = window.scrollY <= parent.offsetHeight;
